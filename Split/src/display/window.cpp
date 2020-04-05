e1524b08ed86e415fca7b46413433930e57ce503
Paul Cento <paul.cento@gmail.com>
2020-04-04 21:19:18 -0400

    Minimal implementation of Window handle abstraction

---
+++ Split/src/display/window.cpp
@@ -0,0 +1,151 @@
+#include "pch.h"
+#include "window.h"
+
+#include "events/window_events.h"
+
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+
+namespace Split
+{
+
+	Window::Window(EventBus* event_bus, int width, int height, std::string title, bool vsync, bool cursor)
+		: EventBusClient(event_bus), m_width{ width }, m_height{ height }, m_title{ title },
+		m_aspect_ratio{ (float)width / (float)height }, m_open{ false }, m_window{ nullptr },
+		m_vsync{ vsync }, m_cursor{ cursor }
+	{
+		
+	}
+
+	Window::Window(EventBus* event_bus)
+	{
+		Window(event_bus, 800, 600, "Split Engine", true, true);
+	}
+
+	Window::~Window()
+	{
+
+	}
+
+	void Window::init(void)
+	{
+		if (glfwInit() == GLFW_FALSE)
+			std::cerr << "glfw init failed.\n";
+
+		m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
+		if (!m_window) {
+			glfwTerminate();
+			std::cerr << "glfw failed to create window.\n";
+		}
+		glfwMakeContextCurrent(m_window);
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+			std::cerr << "gladLoadGLLoader failed.\n";
+
+		glfwSetWindowUserPointer(m_window, this);
+		glfwSetCursorPosCallback(m_window, mouse_move_callback);
+		glfwSetMouseButtonCallback(m_window, mouse_button_callback);
+		glfwSetScrollCallback(m_window, scroll_callback);
+		glfwSetKeyCallback(m_window, key_callback);
+		glfwSetWindowSizeCallback(m_window, window_size_callback);
+		glfwSetWindowCloseCallback(m_window, window_close_callback);
+
+		use_vsync(m_vsync);
+		capture_cursor(!m_cursor);
+
+		glViewport(0, 0, m_width, m_height);
+
+		m_open = true;
+	}
+
+	bool Window::open(void)
+	{
+		return m_open;
+	}
+
+	void Window::update(void)
+	{
+		glfwSwapBuffers(m_window);
+		glfwPollEvents();
+	}
+
+	void Window::capture_cursor(bool capture)
+	{
+		glfwSetInputMode(m_window, GLFW_CURSOR, capture ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL);
+	}
+
+	void Window::use_vsync(bool use)
+	{
+		glfwSwapInterval(use ? 1 : 0);
+	}
+
+	float Window::aspect_ratio(void)
+	{
+		return m_aspect_ratio;
+	}
+
+	/* GLFW callback functions
+	 */
+	static void mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+	{
+		Window* user = (Window*)glfwGetWindowUserPointer(window);
+		// TODO: add mouse struct
+	}
+
+	static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+	{
+		Window* user = (Window*)glfwGetWindowUserPointer(window);
+		switch (action) {
+		case GLFW_PRESS:
+			user->post_event(MousePress(button));
+			break;
+		case GLFW_RELEASE:
+			user->post_event(MouseRelease(button));
+			break;
+		default:
+			break;
+		}
+	}
+
+	void scroll_callback(GLFWwindow* window, double dx, double dy)
+	{
+		Window* user = (Window*)glfwGetWindowUserPointer(window);
+		user->post_event(MouseScroll((float)dx, (float)dy));
+	}
+
+	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		Window* user = (Window*)glfwGetWindowUserPointer(window);
+		switch (action) {
+		case GLFW_PRESS:
+			user->post_event(KeyPress(key));
+			break;
+		case GLFW_REPEAT:
+			user->post_event(KeyRepeat(key));
+			break;
+		case GLFW_RELEASE:
+			user->post_event(KeyRelease(key));
+			break;
+		default:
+			break;
+		}
+	}
+
+	static void window_size_callback(GLFWwindow* window, int width, int height)
+	{
+		Window* user = (Window*)glfwGetWindowUserPointer(window);
+		user->m_width = width;
+		user->m_height = height;
+		user->m_aspect_ratio = (float)width / (float)height;
+		glViewport(0, 0, width, height);
+		user->post_event(WindowResize(width, height));
+	}
+
+	static void window_close_callback(GLFWwindow* window)
+	{
+		Window* user = (Window*)glfwGetWindowUserPointer(window);
+		user->m_open = false;
+		user->post_event(WindowClose());
+	}
+
+}
---
+++ Split/src/display/window.h
@@ -0,0 +1,49 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "events/event_bus_client.h"
+
+/* Forward declare to avoid glfw header
+ */
+struct GLFWwindow;
+
+namespace Split
+{
+
+	class Window : public EventBusClient
+	{
+	public:
+		Window(EventBus* bus, int width, int height, std::string title, bool vsync = true, bool cursor = true);
+		Window(EventBus* bus);
+		~Window();
+
+		void init(void);
+		bool open(void);
+		void update(void);
+		void capture_cursor(bool capture);
+		void use_vsync(bool use);
+		float aspect_ratio(void);
+
+	private:
+		std::string m_title;
+		bool m_open;
+
+		GLFWwindow* m_window;
+
+		int m_width, m_height;
+		float m_aspect_ratio;
+		bool m_vsync, m_cursor;
+
+		void event_bus_subscribe(void) override;
+
+		friend void mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+		friend void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+		friend void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+		friend void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		friend void window_size_callback(GLFWwindow* window, int width, int height);
+		friend void window_close_callback(GLFWwindow* window);
+	};
+
+}
+
+#endif /* WINDOW_H */
